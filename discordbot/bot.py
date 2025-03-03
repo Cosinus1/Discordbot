abad130a5fe57686f615cf6983c16977f8f47e54
Cosinus1 <paulhades225@gmail.com>
2025-03-03 02:29:13 +0100

    events and commands

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -3,6 +3,8 @@ from config import bot, TOKEN
 from database import init_db
 from events import on_ready, on_message, on_member_join, on_voice_state_update
 from tasks import check_inactivity
+from commands.user_commands import *
+from commands.admin_commands import admin, bye
 
 # Initialize the database
 init_db()
@@ -15,6 +17,18 @@ bot.event(on_voice_state_update)
 
 # Register tasks
 check_inactivity.start()
+
+# Register user commands
+bot.add_command(exp)
+bot.add_command(money)
+bot.add_command(bet)
+bot.add_command(daily)
+bot.add_command(send)
+bot.add_command(roll)
+bot.add_command(expvoice)
+# Register admin commands
+bot.add_command(admin)
+bot.add_command(bye)
 
 # Run the bot
 bot.run(TOKEN)
---
+++ discordbot/events/on_member_join.py
@@ -0,0 +1,10 @@
+import discord
+from database import update_user_data
+from utils.roles import get_or_create_role, send_to_bot_channel
+
+async def on_member_join(member):
+    guild = member.guild
+    role_Gueux = await get_or_create_role(guild, "Gueux", discord.Color.dark_grey())
+    await member.add_roles(role_Gueux)
+    update_user_data(member.id, role='Gueux')
+    await send_to_bot_channel(guild, f"{member.name} a reçu le rôle Gueux.")
