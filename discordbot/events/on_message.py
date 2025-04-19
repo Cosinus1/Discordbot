20fed757870d0fc43c4358824e7ae40cd969654b
Cosinus1 <paulhades225@gmail.com>
2025-04-19 20:28:21 +0200

    Update on_message.py

--- discordbot/events/on_message.py
+++ discordbot/events/on_message.py
@@ -80,7 +80,7 @@ async def start(message):
                 # Add recent conversation history as separate messages to maintain the dialogue structure
                 author = msg.author   
                 roles = "bot" if author.bot else "user"
-                roles += " admin" if msg.author.admin else None
+                #roles += " admin" if msg.author.admin else None
                 roles += " master" if 'dev' in [role.name.lower() for role in author.roles] else None
                 context_messages.insert(0, {"author": author.display_name, "roles": roles, "content": msg.content})
                 if len(context_messages) >= MESSAGE_CONTENT_LIMIT-1:
